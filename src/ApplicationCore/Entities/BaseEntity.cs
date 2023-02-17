c93c7c4fa2011ecbf18f069f4e63c98f8dc714a8
IremCerennBulut <iremcrnblt@gmail.com>
2023-02-17 15:41:40 +0300

    entities

---
+++ src/ApplicationCore/Entities/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Entities
+{
+    //Temel class miras alacağımız class.
+    //Get ve seti override edebilmek için virtual tanımlayacağız.
+    //generic repositoryde baseentitye ait olma kısıtlaması için base classı oluşturduk.
+
+    public class BaseEntity
+    {
+        public virtual int Id { get; set; }
+    }
+}
