03f86941dda0f7b22d80b990f0984c89c922ef48
IremCerennBulut <iremcrnblt@gmail.com>
2023-02-20 17:09:25 +0300

    efrepository

---
+++ src/Infrastructure/Data/EFRepository.cs
@@ -0,0 +1,71 @@
+﻿using ApplicationCore.Entities;
+using ApplicationCore.Interfaces;
+using Ardalis.Specification;
+using Ardalis.Specification.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    internal class EFRepository<T> : IRepository<T> where T : BaseEntity //class>interface>constrait
+    {
+        private readonly ShopContext _db;
+
+        public EFRepository(ShopContext db)
+        {
+            _db = db;
+        }
+        public async Task<T> AddAsync(T entity)
+        {
+            _db.Add(entity);
+            await _db.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<int> CountAsync(ISpecification<T> specification)
+        {
+            return await _db.Set<T>().WithSpecification(specification).CountAsync();
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            _db.Remove(entity);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task<T> FirstAsync(ISpecification<T> specification)
+        {
+            return await _db.Set<T>().WithSpecification(specification).FirstAsync();
+        }
+
+        public async Task<T?> FirtOrDefaultAsync(ISpecification<T> specification)
+        {
+            return await _db.Set<T>().WithSpecification(specification).FirstOrDefaultAsync();
+        }
+
+        public async Task<List<T>> GetAllAsync()
+        {
+            return await _db.Set<T>().ToListAsync();
+        }
+
+        public async Task<List<T>> GetAllAsync(ISpecification<T> specification) //spesifik olarak listeleme sorgusu yapılmak istenebilir. ISpesification interfacesi ile süzerek getirebiliriz. 
+        {
+            return await _db.Set<T>().ToListAsync(specification);
+        }
+
+        public async Task<T?> GetByIdAsync(int id)
+        {
+            return await _db.FindAsync<T>(id);
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _db.Update(entity);
+            await _db.SaveChangesAsync();
+        }
+    }
+}
